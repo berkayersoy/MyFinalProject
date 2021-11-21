193f346da78c75ffa0886ebb927364ef7dfc6fa9
berkayersoy <44724944+berkayersoy@users.noreply.github.com>
2021-11-21 22:02:56 +0100

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Codes of work
+
+            //Yetkisi var mi ? if'ler burada.
+
+            //To database - give me the products
+            return _productDal.GetAll();
+        }
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //internal defaultudur
+    //internal Entities icerisinde internal demek
+    //public tum katmanlar erisebilir
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+}
